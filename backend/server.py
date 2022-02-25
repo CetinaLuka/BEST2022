69ab0bec366321070ba766b0cf6c0b2cf9b66520
CetinaLuka <luka.cetina@student.um.si>
2022-02-25 17:21:50 +0000

    ch ch changes

--- backend/server.py
+++ backend/server.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/import')
+def importMeasurements():
+
+    return "Uvazam";
+
 if __name__ == '__main__':
     app.run()
