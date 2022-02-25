21ab39779595d9d30af96eb8b3d441425e30333d
TRavnak <tilen.ravnak@student.um.si>
2022-02-25 19:33:15 +0100

    Prepoznavanje polnjenja1

--- backend/Modules/best2022.py
+++ backend/Modules/best2022.py
@@ -41,14 +41,13 @@ def importAccess():
 def manageData(data):
     averageByDate = data.groupby("Date").mean()
     averageByDate["Diff"] = averageByDate["Oil"].diff() * -1
-    averageByDate["Fill"] = averageByDate["Diff"] < 0
+    averageByDate["Refil"] = (averageByDate["Diff"] < -1) 
+    averageByDate["Data Error"] = (averageByDate["Diff"] < -0.0000001) 
     print(averageByDate.to_string())
 
 #mergeFiles()
-#data = pd.read_csv("../../allData.csv")
-#data = formatData(data)
+data = pd.read_csv("../../allData.csv")
+data = formatData(data)
 
-importAccess()
-manageData(data)
 #importAccess()
-#menageData(data)
+manageData(data)
