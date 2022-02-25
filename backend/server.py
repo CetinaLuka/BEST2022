81ba7d1965835d7c5edd68d46ec3b5030bb143d4
CetinaLuka <luka.cetina@student.um.si>
2022-02-25 20:07:27 +0000

    feat: enabled email

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,50 @@
 import Modules.best2022 as best
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.getenv('MAIL_USERNAME'),
+    "MAIL_PASSWORD": os.getenv('MAIL_PASSWORD')
+}
+
 app = Flask(__name__)
+
+app.config.update(mail_settings)
+mail = Mail(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    print(app.config)
+    return 'Hello World!!!!!'
 
 @app.route('/import')
 def importMeasurements():
     return best.importAccess()
     
-
+@app.route('/email')
 def sendMail():
-    print("sending mail");
+    with app.app_context():
+        print("sending mail");
+        msg = Message(
+            subject="A ste kaj slabi?", 
+            sender=app.config.get("MAIL_USERNAME"), 
+            recipients=["luka.cetina@student.um.si"], 
+            body="Gospod vi ste en velik velik noob. \nLp in lep pozdrav"
+        )
+        mail.send(msg)
+        print("mail sent");
+    return "email poslan"
+
+    
 
 if __name__ == '__main__':
     app.run()
