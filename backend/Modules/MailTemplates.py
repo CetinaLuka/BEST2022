649df68e7368cad2a66d3bdbd4608fcd3ab4c732
CetinaLuka <luka.cetina@student.um.si>
2022-02-26 14:41:25 +0000

    changes

--- backend/Modules/MailTemplates.py
+++ backend/Modules/MailTemplates.py
@@ -17,7 +17,7 @@ def createConsumptionWarning(value, date, recipients=[os.getenv('WARNING_RECIPIE
 def createRefilWarning(value, date, amount, recipients=[os.getenv('WARNING_RECIPIENT')], sender=os.getenv("MAIL_USERNAME")):
     msg = Message(
         subject="Dolivanje kurilnega olja",
-        sender=sender, 
+        sender=sender,
         recipients=recipients,
     )
     msg.html = render_template("zaznano_polnjenje.html", date=date, amount=amount, consumption=value)
